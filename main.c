0a50e06900b1fba4b8cfb987e80ecdb4f4fbc5ae
jakobgokpinar <jakobgokpinar@gmail.com>
2026-01-25 20:42:20 +0100

    world count expanding in multiple buffers

--- main.c
+++ main.c
@@ -1,4 +1,29 @@
+#include <stddef.h>
 #include <unistd.h>
+
+struct wc_state {
+    int in_word;
+    size_t words;
+};
+
+static int is_whitespace(char c) {
+    return c == ' ' || c == '\n' || c == '\t';
+}
+
+void process_buffer(struct wc_state *st, const char *buf, size_t len) {
+    for (size_t i = 0; i < len; i++) {
+        char c = buf[i];
+
+        if (is_whitespace(c)) {
+            st->in_word = 0;
+        } else {
+            if (st->in_word == 0) {
+                st->words++;
+                st->in_word = 1;
+            }
+        }
+    }
+}
 
 void write_int(int x) {
     char buf[12];
@@ -30,15 +55,23 @@ int main(int argc, char *argv[]) {
     ssize_t n;
     size_t total = 0;
     int fd = 0;
-    while ( (n = read(fd, buf, sizeof(buf)) ) > 0) {
-        total += count_chars(buf, n);
+
+    struct wc_state st;
+    st.in_word = 0;
+    st.words = 0;
+
+    while ( (n = read(fd, buf, sizeof(buf))) > 0 ) {
+        process_buffer(&st, buf, (size_t)n);
     }
 
     if (n < 0) {
         write(2, "read error\n", 11);
+        return 1;
     }
 
-    write_int(total);
+    write_int(st.words);
+    write(1, "\n", 1);
+
 
     return 0;
 }
